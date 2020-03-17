6ca539e32daf06e99ab9d99a2bb9ccacbdc53d45
Isanka Wijerathne <isanka@isanka.local>
2020-03-17 12:19:27 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,145 @@
+# COVID19Japan.com
+
+A COVID-19 Coronavirus tracker for Japan. Government agencies release detailed data, but in difficult to read formats and usually only in Japaneese.
+
+
+
+## Contributing Data
+
+If you would like to update any missing or incorrect data on the graphs, please leave a comment on this spreadsheet, and/or request edit access.
+
+https://docs.google.com/spreadsheets/d/1w9ZGTZHEQUF-t_4HQyCL_QOTiobFOzse7s1n25VJk7w/edit
+
+
+
+## Data Sources
+
+**Countrywide Data**
+
+[Ministry of Health, Labour and Welfare](https://www.mhlw.go.jp/stf/houdou/index.html)
+
+Naming of reports from the Ministry of Health Labour and Welfare
+ * Overall Daily Summaries: 新型コロナウイルス感染症の現在の状況と厚生労働省の対応について
+ * Patient Detail Updates: 新型コロナウイルスに関連した患者の発生について
+
+- [Updates 2020/03](https://www.mhlw.go.jp/stf/houdou/houdou_list_202003.html).
+  - [2020-03-02 Aggregate](https://www.mhlw.go.jp/stf/newpage_09889.html)
+  - [2020-03-01 Details 218-232](https://www.mhlw.go.jp/stf/newpage_09864.html)
+
+- [Updates 2020/02](https://www.mhlw.go.jp/stf/houdou/houdou_list_202002.html)
+  - [2020-02-29 Details 209-217](https://www.mhlw.go.jp/stf/newpage_09859.html)
+  - [2020-02-28 Details 192-208](https://www.mhlw.go.jp/stf/newpage_09857.html)
+
+
+NHK Daily Aggregates
+  - [2020-03-04](https://www3.nhk.or.jp/news/html/20200304/k10012312901000.html?utm_int=word_contents_list-items_006&word_result=%E6%96%B0%E5%9E%8B%E3%82%B3%E3%83%AD%E3%83%8A%E3%82%A6%E3%82%A4%E3%83%AB%E3%82%B9)
+
+
+**Prefecture-specific Data**
+|City    |Source    |
+|--------|----------|
+|Aichi    |[新型コロナウイルス感染症患者の発生について](https://www.pref.aichi.jp//soshiki/kenkotaisaku/novel-coronavirus.html)|
+|Akita    ||
+|Aomori    ||
+|Chiba    |[新型コロナウイルスに関連した患者の発生について](https://www.pref.chiba.lg.jp/shippei/kansenshou/keihatu-index.html#an1)|
+|Ehime    ||
+|Fukui    ||
+|Fukuoka    | [](http://www.pref.fukuoka.lg.jp/contents/corona-kokunai.html) |
+|Fukushima    ||
+|Gifu    ||
+|Gunma    ||
+|Hiroshima    ||
+|Hokkaido    |[新型コロナウイルス感染症の道内の発生状況](http://www.pref.hokkaido.lg.jp/hf/kth/kak/hasseijoukyou.htm)|
+|Hyogo    | [新型コロナウイルス感染症の県内検査状況について](https://web.pref.hyogo.lg.jp/kf16/singatakoronakensa.html) |
+|Ibaraki    ||
+|Ishikawa    | [新型コロナウイルス感染症について](https://www.pref.ishikawa.lg.jp/kansen/corona.html) |
+|Iwate    ||
+|Kagawa    ||
+|Kanagawa    |[新型コロナウイルスに感染した患者の発生状況](https://www.pref.kanagawa.jp/docs/ga4/bukanshi/occurrence.html)|
+|Kagoshima    ||
+|Kochi    ||
+|Kumamoto    | [新型コロナウイルス感染症](https://www.pref.kumamoto.jp/hpkiji/pub/List.aspx?c_id=3&class_set_id=1&class_id=7049) |
+|Kyoto    |[新型コロナウイルス感染症の患者の発生について](https://www.pref.kyoto.jp/kentai/news/novelcoronavirus.html#F)|
+|Mie    |[新型コロナウイルス感染症に関連した肺炎患者の発生について](https://www.pref.mie.lg.jp/YAKUMUS/HP/m0068000066.htm)|
+|Miyagi    ||
+|Miyazaki    ||
+|Nagano    | [新型コロナウイルス感染症に係る検査状況について](https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/bukan-haien.html#kennsajoukyou)|
+|Nara    |[新型コロナウイルス感染症の患者の発生について](http://www.pref.nara.jp/module/1356.htm#moduleid1356)|
+|Nagasaki    ||
+|Niigata    ||
+|Oita    ||
+|Okayama    ||
+|Okinawa    | [患者の発生について](https://www.pref.okinawa.jp/site/hoken/chiikihoken/kekkaku/2019-ncov.html)|
+|Osaka    | [新型コロナウイルス感染症について](http://www.pref.osaka.lg.jp/iryo/osakakansensho/corona.html)|
+|Saga    ||
+|Saitama    |[記者発表資料](https://www.pref.saitama.lg.jp/a0701/shingatacoronavirus.html)|
+|Shiga    ||
+|Shimane    ||
+|Shizuoka    ||
+|Tochigi    | [新型コロナウイルス感染症について](http://www.pref.tochigi.lg.jp/e04/welfare/hoken-eisei/kansen/hp/shingatakoronavirus.html) |
+|Tokushima    ||
+|Tokyo    |[新型コロナウイルスに関連した患者の発生](https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/02/index.html)|
+|Tottori    ||
+|Toyama    ||
+|Yamagata    ||
+|Yamaguchi    ||
+|Yamanashi    ||
+|Wakayama    |[新型コロナウイルス感染症に関連する情報について](https://www.pref.wakayama.lg.jp/prefg/000200/covid19.html)|
+
+**We need your help!** Please submit any information sources via the "Issues" tab above. Thank you! ありがとう！
+
+
+## Contributing Code
+
+If you would like to contribute features / refactor / etc, please open an Issue on this repo, and tag @reustle - We can then open a new Pull Request if it is deemed in line with the projects goals.
+
+### Requirements
+
+ * NodeJS
+
+### Build Instructions
+
+Set up environment
+```
+npm install
+```
+
+Build once:
+```
+npm run build
+```
+
+Build continuously:
+```
+npm run watch
+```
+
+Start Server:
+```
+npm run start
+
+# or run continuous build + server
+npm run start-webpack
+```
+
+Build for production (minified):
+```
+npm run build-prod
+```
+
+## Contributors
+
+- [Shane Reustle](https://reustle.org): Development
+- [Jiahui Zhou](https://jiahuizhou.design/): Concept, Design, and Data
+- and [these developers](https://github.com/reustle/covid19japan/graphs/contributors)
+
+## Other Resources
+- [都道府県別新型コロナウイルス感染者数マッ (Arcgis)](https://jagjapan.maps.arcgis.com/apps/opsdashboard/index.html#/259ce3e3e2bf4c77876d4ecde6ea2564)
+- [Data source from John Hopkins CSSE](https://github.com/CSSEGISandData/COVID-19)
+- [News Digest Tracker](https://newsdigest.jp/pages/coronavirus/) (Unclear where the data is coming from)
+- [BNO News Global Tracker](https://bnonews.com/index.php/2020/02/the-latest-coronavirus-cases/) ([Google My Maps View](https://www.google.com/maps/d/u/0/viewer?mid=1a04iBi41DznkMaQRnICO40ktROfnMfMx&ll=37.45239782566561%2C137.6798301595186&z=6))
+- [COVID-19 日本速報](https://japan-cov-19.now.sh/) ([Data Source](https://docs.google.com/spreadsheets/d/1HzOdYwWxSNmjHqDOYp3rq9tn0_vIILvRA47OF3DtKqc/edit#gid=1123835558))
+- [Toyo Keizai Situation Report](https://toyokeizai.net/sp/visual/tko/covid19/) (Data from MHLW)
+- [Patient connections and clusters map in Japan 新型コロナウイルスの事例マップ](https://www.coromap.info/)
+- Wikipedia on 2020 Coronavirus Outbreak in Japan [English](https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_Japan) and [Japanese](https://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AB%E3%81%8A%E3%81%91%E3%82%8B2019%E5%B9%B4%E3%82%B3%E3%83%AD%E3%83%8A%E3%82%A6%E3%82%A4%E3%83%AB%E3%82%B9%E6%84%9F%E6%9F%93%E7%97%87%E3%81%AE%E6%B5%81%E8%A1%8C%E7%8A%B6%E6%B3%81) - Sources
+- [@covid19japan on Twitter (Suspended)](https://twitter.com/covid19japan)
---
+++ src/index.js
@@ -0,0 +1,533 @@
+mapboxgl.accessToken =
+    'pk.eyJ1IjoiaXNhbmthZG4iLCJhIjoiY2s3dmIxYmxhMGJ2OTNmbzJnZjFxcmF6ZyJ9.Pgj6f8H7NpCRI1fNOK3MfA'
+const PREFECTURE_JSON_PATH = 'static/prefectures.geojson'
+const JSON_PATH = 'https://covid19sl.s3.amazonaws.com/data.json'
+const TIME_FORMAT = 'YYYY-MM-DD'
+const COLOR_CONFIRMED = 'rgb(244,67,54)'
+const COLOR_RECOVERED = 'rgb(25,118,210)'
+const COLOR_DECEASED = 'rgb(55,71,79)'
+const COLOR_INCREASE = 'rgb(163,172,191)'
+const PAGE_TITLE = 'Coronavirus Disease (COVID-19) Japan Tracker'
+let LANG = 'en'
+
+// Global vars
+let ddb = {
+  prefectures: undefined,
+  trend: undefined,
+  totals: {
+    confirmed: 0,
+    recovered: 0,
+    deceased: 0,
+    tested: 0,
+    critical: 0
+  },
+  totalsDiff: {
+    confirmed: 0,
+    recovered: 0,
+    deceased: 0,
+    tested: 0,
+    critical: 0
+  }
+}
+let map = undefined
+
+
+// IE11 forEach Polyfill
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+  console.info('polyfill for IE11');
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
+
+
+function loadData(callback) {
+  // Load the json data file
+
+  fetch(JSON_PATH)
+  .then(function(res){
+    return res.json()
+  })
+  .then(function(data){
+    callback(data)
+  })
+}
+
+
+function calculateTotals(daily) {
+  // Calculate the totals
+
+  let totals = {
+    confirmed: 0,
+    recovered: 0,
+    deceased: 0,
+    critical: 0,
+    tested: 0
+  }
+  let totalsDiff = {
+    confirmed: 1,
+    recovered: 1,
+    deceased: 1,
+    critical: 1,
+    tested: 1
+  }
+
+  // If there is an empty cell, fall back to the previous row
+  function pullLatestSumAndDiff(key) {
+    if(daily[daily.length-1][key].length){
+      totals[key] = parseInt(daily[daily.length-1][key])
+      totalsDiff[key] = totals[key] - parseInt(daily[daily.length-2][key])
+    }else{
+      totals[key] = parseInt(daily[daily.length-2][key])
+      totalsDiff[key] = totals[key] - parseInt(daily[daily.length-3][key])
+    }
+  }
+
+  pullLatestSumAndDiff('tested')
+  pullLatestSumAndDiff('critical')
+  pullLatestSumAndDiff('confirmed')
+  pullLatestSumAndDiff('recovered')
+  pullLatestSumAndDiff('deceased')
+
+  return [totals, totalsDiff]
+}
+
+
+function drawMap() {
+  // Initialize Map
+
+  map = new mapboxgl.Map({
+    container: 'map-container',
+    style: 'mapbox://styles/mapbox/light-v10',
+    zoom: 4,
+    minZoom: 3.5,
+    maxZoom: 7,
+    center: {
+      lng: 139.11792973051274,
+      lat: 38.52245616545571
+    },
+    maxBounds: [
+      {lat: 12.118318014416644, lng: 100.01240618330542}, // SW
+      {lat: 59.34721256263214, lng: 175.3273570446982} // NE
+    ]
+  })
+
+  map.dragRotate.disable()
+  map.touchZoomRotate.disableRotation()
+  map.scrollZoom.disable()
+  map.addControl(new mapboxgl.NavigationControl({
+    showCompass: false,
+    showZoom: true
+  }))
+}
+
+
+function drawTrendChart(sheetTrend) {
+
+  let lastUpdated = ''
+  let labelSet = []
+  let confirmedSet = []
+  let recoveredSet = []
+  let deceasedSet = []
+  let dailyIncreaseSet = []
+
+  let prevConfirmed = -1
+  sheetTrend.map(function(trendData){
+    labelSet.push(new Date(trendData.date))
+    confirmedSet.push({
+      x: new Date(trendData.date),
+      y: parseInt(trendData.confirmed)
+    })
+    recoveredSet.push({
+      x: new Date(trendData.date),
+      y: parseInt(trendData.recovered)
+    })
+    deceasedSet.push({
+      x: new Date(trendData.date),
+      y: parseInt(trendData.deceased)
+    })
+    dailyIncreaseSet.push({
+      x: new Date(trendData.date),
+      y: prevConfirmed === -1 ? 0 : parseInt(trendData.confirmed) - prevConfirmed
+    })
+
+    prevConfirmed = parseInt(trendData.confirmed)
+    lastUpdated = trendData.date
+  })
+
+  var ctx = document.getElementById('trend-chart').getContext('2d')
+  Chart.defaults.global.defaultFontFamily = "'Open Sans', helvetica, sans-serif"
+  Chart.defaults.global.defaultFontSize = 16
+  Chart.defaults.global.defaultFontColor = 'rgb(0,10,18)'
+
+  var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labelSet,
+      datasets: [
+        {
+          label: 'Deceased',
+          borderColor: COLOR_DECEASED,
+          backgroundColor: COLOR_DECEASED,
+          fill: false,
+          data: deceasedSet
+        },
+        {
+          label: 'Recovered',
+          borderColor: COLOR_RECOVERED,
+          backgroundColor: COLOR_RECOVERED,
+          fill: false,
+          data: recoveredSet
+        },
+        {
+          label: 'Confirmed',
+          borderColor: COLOR_CONFIRMED,
+          backgroundColor: COLOR_CONFIRMED,
+          fill: false,
+          data: confirmedSet
+        },
+        {
+          label: 'Daily Increase',
+          borderColor: COLOR_INCREASE,
+          backgroundColor: COLOR_INCREASE,
+          fill: false,
+          data: dailyIncreaseSet
+        }
+      ]
+    },
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      elements: {
+        line: {
+          tension: 0.1
+        }
+      },
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {
+            parser: TIME_FORMAT,
+            round: 'day',
+            tooltipFormat: 'll'
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
+          }
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Cases'
+          }
+        }]
+      }
+    }
+  });
+}
+
+
+function drawPrefectureTable(prefectures, totals) {
+  // Draw the Cases By Prefecture table
+
+  let dataTable = document.querySelector('#prefectures-table tbody')
+  let unspecifiedRow = ''
+
+  // Remove the loading cell
+  dataTable.innerHTML = ''
+
+  // Parse values so we can sort
+  _.map(prefectures, function(pref){
+    // TODO change to confirmed
+    pref.confirmed = (pref.cases?parseInt(pref.cases):0)
+    pref.recovered = (pref.recovered?parseInt(pref.recovered):0)
+    // TODO change to deceased
+    pref.deceased = (pref.deaths?parseInt(pref.deaths):0)
+  })
+
+  // Iterate through and render table rows
+  _.orderBy(prefectures, 'confirmed', 'desc').map(function(pref){
+    if(!pref.confirmed && !pref.recovered && !pref.deceased){
+      return
+    }
+
+    let prefStr
+    if(LANG == 'en'){
+        prefStr = pref.prefecture
+    }else{
+      prefStr = pref.prefectureja
+    }
+
+    // TODO Make this pretty
+
+    if(pref.prefecture == 'Unspecified'){
+      // Save the "Unspecified" row for the end of the table
+      unspecifiedRow = "<tr><td><em>" + prefStr + "</em></td><td>" + pref.confirmed + "</td><td>" + pref.recovered + "</td><td>" + pref.deaths + "</td></tr>"
+    }else if (pref.prefecture == 'Total'){
+      // Skip
+    }else{
+      dataTable.innerHTML = dataTable.innerHTML + "<tr><td>" + prefStr + "</td><td>" + pref.confirmed + "</td><td></td><td>" + (pref.deceased?pref.deceased:'') + "</td></tr>"
+    }
+    return true
+  })
+
+  dataTable.innerHTML = dataTable.innerHTML + unspecifiedRow
+
+  let totalStr = 'Total'
+  if(LANG == 'ja'){
+    totalStr = '計'
+  }
+
+  dataTable.innerHTML = dataTable.innerHTML + "<tr class='totals'><td>" + totalStr + "</td><td>" + totals.confirmed + "</td><td>" + totals.recovered + "</td><td>" + totals.deceased + "</td></tr>"
+}
+
+
+function drawKpis(totals, totalsDiff) {
+  // Draw the KPI values
+
+  function setKpi(key, value) {
+    document.querySelector('#kpi-' + key + ' .value').innerHTML = value
+  }
+  function setKpiDiff(key, value) {
+    let diffDir = (value >= 0?'+':'')
+    document.querySelector('#kpi-' + key + ' .diff').innerHTML = '( ' + diffDir + value + ' )'
+  }
+
+  setKpi('confirmed', totals.confirmed)
+  setKpiDiff('confirmed', totalsDiff.confirmed)
+  setKpi('recovered', totals.recovered)
+  setKpiDiff('recovered', totalsDiff.recovered)
+  setKpi('deceased', totals.deceased)
+  setKpiDiff('deceased', totalsDiff.deceased)
+  setKpi('critical', totals.critical)
+  setKpiDiff('critical', totalsDiff.critical)
+  setKpi('tested', totals.tested)
+  setKpiDiff('tested', totalsDiff.tested)
+  setKpi('active', (totals.confirmed - totals.recovered) - totals.deceased)
+  setKpiDiff('active', (totalsDiff.confirmed - totalsDiff.recovered) - totalsDiff.deceased)
+
+}
+
+
+function drawLastUpdated(lastUpdated) {
+  // Draw the last updated time
+
+  // TODO we should be parsing the date, but I
+  // don't trust the user input on the sheet
+  //let prettyUpdatedTime = moment(lastUpdated).format('MMM D, YYYY') + ' JST'
+  document.getElementById('last-updated').innerHTML = lastUpdated
+}
+
+
+function drawPageTitleCount(confirmed) {
+  // Update the number of confirmed cases in the title
+
+  document.title = "(" + confirmed + ") " + PAGE_TITLE
+}
+
+/**
+ * drawMapPrefectures
+ * @param {*} pageDraws - number of redraws to screen
+ */
+function drawMapPrefectures(pageDraws) {
+  // Find the index of the first symbol layer
+  // in the map style so we can draw the
+  // prefecture colors behind it
+
+  var firstSymbolId
+  var layers = map.getStyle().layers
+  for(var i = 0; i < layers.length; i++) {
+    if(layers[i].type === 'symbol') {
+      firstSymbolId = layers[i].id
+      break;
+    }
+  }
+
+  // Start the Mapbox search expression
+  let prefecturePaint = [
+    'match',
+    ['get', 'NAME_1'],
+  ]
+
+  // Go through all prefectures looking for cases
+  ddb.prefectures.map(function(prefecture){
+
+    let cases = parseInt(prefecture.cases)
+    if(cases > 0){
+      prefecturePaint.push(prefecture.prefecture)
+
+      if(cases <= 10){
+        // 1-10 cases
+        prefecturePaint.push('rgb(253,234,203)')
+      }else if(cases <= 25){
+        // 11-25 cases
+        prefecturePaint.push('rgb(251,155,127)')
+      }else if(cases <= 50){
+        // 26-50 cases
+        prefecturePaint.push('rgb(244,67,54)')
+      }else{
+        // 51+ cases
+        prefecturePaint.push('rgb(186,0,13)')
+      }
+    }
+
+  })
+
+  // Add a final value to the list for the default color
+  prefecturePaint.push('rgba(0,0,0,0)')
+
+
+  if (pageDraws === 0) {
+    // If it is the first time drawing the map
+
+    map.addSource('prefectures', {
+      type: 'geojson',
+      data: PREFECTURE_JSON_PATH,
+    })
+
+    // Add the prefecture color layer to the map
+    map.addLayer({
+      'id': 'prefecture-layer',
+      'type': 'fill',
+      'source': 'prefectures',
+      'layout': {},
+      'paint': {
+        'fill-color': prefecturePaint,
+        'fill-opacity': 0.8
+      }
+    }, firstSymbolId)
+
+    // Add another layer with type "line"
+    // to provide a styled prefecture border
+    let prefBorderLayer = map.addLayer({
+      'id': 'prefecture-outline-layer',
+      'type': 'line',
+      'source': 'prefectures',
+      'layout': {},
+      'paint': {
+        'line-width': 0.5,
+        'line-color': '#c0c0c0',
+        'line-opacity': 0.5
+      }
+    }, firstSymbolId)
+
+  } else {
+    // Update prefecture paint properties
+
+    map.setPaintProperty('prefecture-layer', 'fill-color', prefecturePaint)
+
+  }
+}
+
+function initDataTranslate() {
+  // Handle language switching using data params
+
+  const selector = '[data-ja]'
+  const parseNode = function(cb) {
+    document.querySelectorAll(selector).forEach(cb)
+  }
+
+  // Default website is in English. Extract it as the attr data-en="..."
+  parseNode(function(el) {
+    el.dataset['en'] = el.textContent
+  })
+
+  // Language selector event handler
+  document.querySelectorAll('[data-lang-picker]').forEach(function(pick) {
+    pick.addEventListener('click', function(e){
+      e.preventDefault()
+      LANG = e.target.dataset.langPicker
+
+      // Toggle the html lang tags
+      parseNode(function(el) {
+        if (!el.dataset[LANG]) return;
+        el.textContent = el.dataset[LANG]
+      })
+
+      // Update the map
+      map.getStyle().layers.forEach(function(thisLayer){
+        if(thisLayer.type == 'symbol'){
+          map.setLayoutProperty(thisLayer.id, 'text-field', ['get','name_' + LANG])
+        }
+      })
+
+      // Redraw the prefectures table
+      if(document.getElementById('prefectures-table')){
+        drawPrefectureTable(ddb.prefectures, ddb.totals)
+      }
+
+      // Toggle the lang picker
+      document.querySelectorAll('a[data-lang-picker]').forEach(function(el){
+        el.style.display = 'inline'
+      })
+      document.querySelector('a[data-lang-picker='+LANG+']').style.display = 'none'
+
+    })
+  })
+}
+
+window.onload = function(){
+
+  initDataTranslate()
+  drawMap()
+
+  var pageDraws = 0
+  var styleLoaded = false
+  var jsonData = undefined
+  const FIVE_MINUTES_IN_MS = 300000
+
+  function whenMapAndDataReady(){
+    // This runs drawMapPref only when
+    // both style and json data are ready
+
+    if(!styleLoaded || !jsonData){
+      return
+    }
+
+    drawMapPrefectures(pageDraws)
+  }
+
+  map.once('style.load', function(e) {
+    styleLoaded = true
+    whenMapAndDataReady()
+  })
+
+  function loadDataOnPage() {
+    loadData(function(data) {
+      jsonData = data
+
+      ddb.prefectures = jsonData.prefectures
+      let newTotals = calculateTotals(jsonData.daily)
+      ddb.totals = newTotals[0]
+      ddb.totalsDiff = newTotals[1]
+      ddb.trend = jsonData.daily
+      ddb.lastUpdated = jsonData.updated[0].lastupdated
+
+      drawKpis(ddb.totals, ddb.totalsDiff)
+      if (!document.body.classList.contains('embed-mode')) {
+        drawLastUpdated(ddb.lastUpdated)
+        drawPageTitleCount(ddb.totals.confirmed)
+        drawPrefectureTable(ddb.prefectures, ddb.totals)
+        drawTrendChart(ddb.trend)
+      }
+
+      whenMapAndDataReady()
+    })
+  }
+
+  loadDataOnPage()
+
+  // Reload data every INTERVAL
+  setInterval(function() {
+    pageDraws++
+    loadDataOnPage()
+  }, FIVE_MINUTES_IN_MS)
+}
