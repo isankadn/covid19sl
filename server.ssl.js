33864b8b91bf0e5ac40b0534c4ccf0666606153d
Isanka Wijerathne <isanka@isanka.local>
2020-03-22 03:42:49 +0900

    ssl

--- server.ssl.js
+++ server.ssl.js
@@ -4,7 +4,9 @@ const app = express()
 const http = require('http')
 const https = require('https')
 const fs = require('fs')
-
+// const querystring = require('querystring')
+// const compression = require('compression')
+// const bodyParser = require('body-parser')
 
 
 // app.use(compression()); // GZIP http sent files for performance
@@ -12,20 +14,6 @@ app.use(express.static('docs', { dotfiles: 'allow' }))
 // app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
 app.listen(80)
-
-// https
-//     .createServer(
-//         {
-//             key: fs.readFileSync(
-//                 '/etc/letsencrypt/live/covidsl.com/fullchain.pem'
-//             ),
-//             cert: fs.readFileSync(
-//                 '/etc/letsencrypt/live/covidsl.com/privkey.pem'
-//             )
-//         },
-//         app
-//     )
-//     .listen(443)
 
 https
     .createServer(
@@ -39,6 +27,5 @@ https
         },
         app
     )
-    .listen(app.get('port'), function() {
-        console.log('Express SSL server listening on port ' + app.get('port'))
-    })
+    .listen(443)
+
