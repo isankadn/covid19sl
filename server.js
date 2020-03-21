5d853cf635da0a8b8ecef9261d080fcc7952558f
Isanka Wijerathne <isanka@isanka.local>
2020-03-22 02:53:38 +0900

    ssl

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-/* eslint-disable no-console */
+// /* eslint-disable no-console */
+// const express = require('express')
+
+// const app = express()
+// app.use(express.static('docs', { dotfiles: 'allow' }))
+
+// let port = process.env.PORT || 4000
+
+// var listener = app.listen(port, function () {
+//   console.log('listening on port ' + listener.address().port);
+//   console.log('http://localhost:' + listener.address().port);
+// });
+
+
 const express = require('express')
-
+const path = require('path')
 const app = express()
-app.use(express.static('docs', { dotfiles: 'allow' }))
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+const querystring = require('querystring')
+const compression = require('compression')
+const bodyParser = require('body-parser')
 
-let port = process.env.PORT || 4000
+app.use(compression())
+app.use(bodyParser.json())
 
-var listener = app.listen(port, function () {
-  console.log('listening on port ' + listener.address().port);
-  console.log('http://localhost:' + listener.address().port);
-});
+app.listen(80)
+
+https.createServer({
+        key: fs.readFileSync("/etc/letsencrypt/live/covidsl.com/fullchain.pem"),
+        cert: fs.readFileSync("/etc/letsencrypt/live/covidsl.com/privkey.pem")
+}, app).listen(443);
