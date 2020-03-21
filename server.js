002398fff5bc1e9b66950c5f9a4db230475982ae
Isanka Wijerathne <isanka@isanka.local>
2020-03-22 03:14:53 +0900

    ssl

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 app.use(express.static('docs'))
 
-let port = process.env.PORT || 80
+let port = process.env.PORT || 4000
 
 var listener = app.listen(port, function () {
   console.log('listening on port ' + listener.address().port);
