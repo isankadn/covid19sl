901d032ba60a8a01df298cce120cb3170204fd73
Isanka Wijerathne <isanka@isanka.local>
2020-03-22 02:41:25 +0900

    dot file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 
 const app = express()
-app.use(express.static('docs'))
+app.use(express.static('docs', { dotfiles: 'allow' }))
 
 let port = process.env.PORT || 4000
 
